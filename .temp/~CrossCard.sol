793681f55d3e3d7b3cb888287c4063bc6393ba7d
Goraniya Karan <karangoraniya@gmail.com>
2023-12-11 09:00:50 +0530

    first commit

---
+++ .temp/~CrossCard.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "/Users/karangoraniya/Study/hackathon/crossCard/node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "/Users/karangoraniya/Study/hackathon/crossCard/node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract CrossCard is ERC721URIStorage, Ownable {
+    string constant TOKEN_URI =
+        "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    uint256 internal tokenId;
+
+     constructor(address initialOwner) ERC721("CrossCard", "MNFT") {
+        _mint(initialOwner);
+    }
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, TOKEN_URI);
+        tokenId++;
+    }
+}
